c235249aaba5add35d924ebf886dcf27d9de699c
Katharina-Chiu <katharinachiu@gmail.com>
2021-02-26 07:31:48 -0800

    setting up for codegrade

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+//adding in a comment here
 
 
 
@@ -29,7 +29,7 @@ myFunction();
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
 function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
+  /putting in something here to submit 
 
   }
  
